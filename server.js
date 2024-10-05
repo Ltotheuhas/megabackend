330c7549798e901a76db7a16454eb71fd55a6d19
Ltotheuhas <lkokorovic@hotmail.com>
2024-10-05 10:58:36 +0200

    auf server

--- server.js
+++ server.js
@@ -118,6 +118,6 @@ app.get('/health', (req, res) => {
 
 // Start the server
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
